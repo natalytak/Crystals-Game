4c452ca2e16fc9de6c70dc19129c195b01aa2442
Tasha <natalytak@yahoo.com>
2019-02-10 01:00:41 -0500

    final css done

--- crystals.js
+++ crystals.js
@@ -6,10 +6,7 @@ $(document).ready(function () {
     $container.css('background', '#def4fa');
 
     var randomNumber;
-    var number1;
-    var number2;
-    var number3;
-    var number4;
+    var arr = [];
     var score = 0;
     var wins = 0;
     var losses = 0;
@@ -18,18 +15,23 @@ $(document).ready(function () {
     $("#random-number").text(randomNumber);
     $("#your-score").text(score);
 
-
+    function getNumber(){
+        var min = 1;
+        var max = 12;
+        while(arr.length < 4){
+        var numbers = Math.floor(Math.random()* (max - min)) + min;
+        if(arr.indexOf(numbers) === -1) arr.push(numbers);
+        }
+    };
 
     function reset() {
         score = 0;
+        arr = [];
         $("#your-score").text(score);
         randomNumber = 19 + Math.floor(Math.random() * 100);
         $("#random-number").text(randomNumber);
-        number1 = 1 + Math.floor(Math.random() * 12);
-        number2 = 1 + Math.floor(Math.random() * 12);
-        number3 = 1 + Math.floor(Math.random() * 12);
-        number4 = 1 + Math.floor(Math.random() * 12);
-        console.log(randomNumber, number1, number2, number3, number4);
+        getNumber();
+        console.log(randomNumber, arr);
  
     };
     reset();
@@ -54,25 +56,25 @@ $(document).ready(function () {
 
 
     $("#image1").on("click", function () {
-        score += number1;
+        score += arr[0];
         $("#your-score").text(score);
         checkScore();
     });
 
     $("#image2").on("click", function () {
-        score += number2;
+        score += arr[1];
         $("#your-score").text(score);
         checkScore();
     });
 
     $("#image3").on("click", function () {
-        score += number3;
+        score += arr[2];
         $("#your-score").text(score);
         checkScore();
     });
 
     $("#image4").on("click", function () {
-        score += number4;
+        score += arr[3];
         $("#your-score").text(score);
         checkScore();
     });
